dea5a5d347ab58c61c6e88bc5392c437209085cd
Philipp Scheede <p.scheede@gmx.de>
2021-01-12 12:41:43 +0100

    style(HierarchyViewController): Apply auto formatting - BUS-166

--- Assembus/Assets/Scripts/MainScreen/Sidebar/HierarchyView/HierarchyViewController.cs
+++ Assembus/Assets/Scripts/MainScreen/Sidebar/HierarchyView/HierarchyViewController.cs
@@ -247,10 +247,10 @@ namespace MainScreen.Sidebar.HierarchyView
 
             // initialize the item
             itemController.Initialize(item, depth);
-            
+
             // do not expand the item if it is a fused group on initialization
-            if(itemController.itemInfo.ItemInfo.isFused) itemController.ExpandItem(false);
-            
+            if (itemController.itemInfo.ItemInfo.isFused) itemController.ExpandItem(false);
+
             return itemController;
         }
 
